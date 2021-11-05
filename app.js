60ddd302645274c6e055277d7d081f0d956dd495
recep-repo <recepedurmus@gmail.com>
2021-11-06 00:53:45 +0300

    js changed

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const counter = document.querySelector(".counter");
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
-const increase = document.getElementById("increase");
 
-decrease.addEventListener('click', () =>{
-    counter.innerHTML = Number(counter.innerHTML) -1 ;
-});
+const number = document.querySelector(".counter");
+const buttons = document.querySelectorAll(".btn");
+let count = 0;
 
-reset.addEventListener('click', () => {
-    counter.innerHTML = 0;
-});
 
-increase.addEventListener('click', () => {
-    counter.innerHTML = Number(counter.innerHTML) + 1 ;
-});
+buttons.forEach(function(btn){
+    btn.addEventListener("click", function(e) {
+        const classin = e.currentTarget.classList;
+        if(classin==("btn decrease")){
+            count--;
+        }else if (classin.contains("increase")){
+            count++;
+        }else{
+            count = 0
+        }
+        if(count<0){
+            number.style.color = "red";
+        }else if(count>0){
+            number.style.color = "green";
+        }else {
+            number.style.color = "white";
+        }
+       
+        number.innerHTML = count;
+
+    } )
+})
